4bfe3b489f01b7142ae4687054eaeb0545bc4610
K.VenkataSneha <sneha.k@thundersoft.com>
2022-08-01 18:28:08 +0530

    assignment

---
+++ large_of_two.py
@@ -0,0 +1,18 @@
+def fun(a,b):
+    if(a>b):
+        print("a is larger")
+    else:
+        print("b is larger")
+    return
+
+
+a=input("Enter a = ")
+b=input("Enter b = ")
+fun(a,b)
+
+
+#output:
+#Enter a = 3
+#Enter b = 5
+#b is larger
+
---
+++ patterns/alpha_pattern.py
@@ -0,0 +1,28 @@
+rows=int(input("Enter the number of rows: "))
+print(rows,end="\n\n")
+ch=97
+print("The pattern is :")
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        n = chr(ch)
+        print(n,end=" ")
+    ch=ch+1  
+    print("\n")   
+
+
+#output:
+#Enter the number of rows: 5
+#5
+
+#The pattern is :
+#a 
+
+#b b 
+
+#c c c 
+
+#d d d d 
+
+#e e e e e 
+    
+
